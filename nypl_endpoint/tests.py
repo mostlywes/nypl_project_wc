ccd01605da8efac448551a43073fe12f549d6450
mostlywes <98903226+mostlywes@users.noreply.github.com>
2022-02-02 16:40:34 -0500

    Update tests.py

--- nypl_endpoint/tests.py
+++ nypl_endpoint/tests.py
@@ -6,11 +6,11 @@ from django.test import TestCase
 class RandomItemViewTests(TestCase):
 
     def test_malformed_url(self):
-        response = self.client.get('/random/?type_of_resource')
+        response = self.client.get('https://nypl-project-wc.herokuapp.com/random/?type_of_resource')
         self.assertEqual(response.status_code, 400)
 
     def test_url_parameter_returns_right_capture(self):
         parameter = "still image"
-        response = self.client.get('/random/?type_of_resource=' + parameter)
+        response = self.client.get('https://nypl-project-wc.herokuapp.com/random/?type_of_resource=' + parameter)
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, parameter)
